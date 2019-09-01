e72aad9a51a4f05adc5205ffd4e8ad7f6a6b3f22
Kang Oka <51883097+kangoka@users.noreply.github.com>
2019-09-01 14:25:43 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('TOD BOT IS HERE NJING');
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pong');
+    	message.reply('PANG PING PANG PING MATAMU COK');
   	}
 });
 
