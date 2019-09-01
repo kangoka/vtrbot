2cab6f6548d8d386400cf83b9b8f8b42b01b12df
Kang Oka <51883097+kangoka@users.noreply.github.com>
2019-09-01 16:22:04 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,30 @@ client.on('message', message => {
     if (message.content.toLowerCase() === 'ping') {
     	message.reply('PANG PING PANG PING MATAMU COK');
   	}
+    else if (message.content.toLowerCase() === 'asu' || message.content.toLowerCase() === 'asw' || message.content.toLowerCase() === 'ajg' || message.content.toLowerCase() === 'anjing') {
+    	message.reply('MUKELU KEK ANJING');
+  	}
+    else if (message.content.toLowerCase() === 'ngentot' || message.content.toLowerCase() === 'ngentod') {
+    	message.reply('NGENTAT NGENTOT, BOCIL MASIH COKIL AE KASAR BET ANJING');
+  	}
+    else if (message.content.toLowerCase() === 'bangsat' || message.content.toLowerCase() === 'bgst' || message.content.toLowerCase() === 'bangsad' || message.content.toLowerCase() === 'bgsd') {
+    	message.reply('HEH ANJING KASAR BANGET NGENTOT');
+  	}
+    else if (message.content.toLowerCase() === 'lonte') {
+    	message.reply('LONTE? MUKAMU KEK LONTE');
+  	}
+    else if (message.content.toLowerCase() === 'kontol' || message.content.toLowerCase() === 'kntl') {
+    	message.reply('KONTOLMU BELOK');
+  	}
+    else if (message.content.toLowerCase() === 'memek' || message.content.toLowerCase() === 'kemem' || message.content.toLowerCase() === 'mmk' || message.content.toLowerCase() === 'mmq') {
+    	message.reply('MEMEK MU TEMBEM');
+  	}
+    else if (message.content.toLowerCase() === 'kentod') {
+    	message.reply('MANGGIL LU TOD <@267473212626042882>');
+  	}
+    else if (message.content.toLowerCase() === 'nigga' || message.content.toLowerCase() === 'nigg' || message.content.toLowerCase() === 'nigger') {
+    	message.reply('GABOLEH RASIS ANJING, YA GAK <@267473212626042882>');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
