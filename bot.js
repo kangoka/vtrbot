59a83a9f5760e0daf65b1fae2200f8f71f888d80
Kang Oka <51883097+kangoka@users.noreply.github.com>
2019-09-01 15:08:39 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,35 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'kontol') {
+    	message.reply('KONTOLMU BELOK');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'lonte') {
+    	message.reply('WAJAHMU KEK LONTE');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'anjing') {
+    	message.reply('MUKALU KEK ANJING');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bangsat') {
+    	message.reply('KASAR BET SIANJING');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bgst') {
+    	message.reply('KASAR BET KEK TITIT LU');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
