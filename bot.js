55e4e52c793c5e43f36b0e1b418eedd15ff45ed5
Kang Oka <51883097+kangoka@users.noreply.github.com>
2019-09-01 15:43:54 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,47 +11,5 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === 'asu' || message.content === 'Asu' || message.content === 'ASU' || message.content === 'Anjing' || message.content === 'ANJING') || message.content === 'Ajg' || message.content === 'AJG' {
-    	message.reply('KASAR BET SIH ANJING');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'kontol' || message.content === 'Kontol' || message.content === 'KONTOL' || message.content === 'kntl' || message.content === 'Kntl' || message.content === 'KNTL' ) {
-    	message.reply('KONTOLMU BELOK');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'lonte' || message.content === 'Lonte' || message.content === 'LONTE' ) {
-    	message.reply('WAJAHMU KEK LONTE');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'bangsat' || message.content === 'bgst' || message.content === 'BANGSAT' || message.content === 'BGST' || message.content === 'Bangsat') || message.content === 'Bgst' {
-    	message.reply('KASAR BET SIANJING');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'ngentot' || message.content === 'ngentod' || message.content === 'Ngentot' || message.content === 'NGENTOT' || message.content === 'Ngentod' || message.content === 'NGENTOD' ) {
-    	message.reply('NGENTAT NGENTOT, BOCIL MASIH COLI AE KASAR BANGET ANJING');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'kentod' || message.content === 'Kentod' || message.content === 'KENTOD') {
-    	message.reply('MANGGIL LU TOD <@267473212626042882>');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'nigg' || message.content === 'Nigg') || message.content === 'NIGG' || message.content === 'nigger' || message.content === 'Nigger' || message.content === 'NIGGER' || message.content === 'nigga' || message.content === 'Nigga' || message.content === 'NIGGA' {
-    	message.reply('GABOLEH RASIS ANJING, YA GAK <@267473212626042882>? ');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
