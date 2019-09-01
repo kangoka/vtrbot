3319e65a08855e5a10435b14257ddf0b8fae06f5
Kang Oka <51883097+kangoka@users.noreply.github.com>
2019-09-01 18:35:01 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,37 +12,46 @@ client.on('message', message => {
     	message.reply('BERISIK ANJING');
   	}
     else if (message.content.toLowerCase().includes ('asu') || ('asw')) {
+    	message.reply('MUKELU KEK ASU');
+  	}
+    else if (message.content.toLowerCase().includes ('ajg') || ('anjing') || ('anjeng')) {
     	message.reply('MUKELU KEK ANJING');
   	}
-    else if (message.content.toLowerCase() === 'ngentot' || message.content.toLowerCase() === 'ngentod') {
+    else if (message.content.toLowerCase().includes ('ngentot') || ('ngentod') || ('ngntd')) {
     	message.reply('NGENTAT NGENTOT, BOCIL MASIH COKIL AE KASAR BET ANJING');
   	}
-    else if (message.content.toLowerCase() === 'bangsat' || message.content.toLowerCase() === 'bgst' || message.content.toLowerCase() === 'bangsad' || message.content.toLowerCase() === 'bgsd') {
+    else if (message.content.toLowerCase().includes ('bgst') || ('bangsat')) {
     	message.reply('HEH ANJING KASAR BANGET NGENTOT');
   	}
-    else if (message.content.toLowerCase() === 'lonte') {
+    else if (message.content.toLowerCase().includes ('lonte')) {
     	message.reply('LONTE? MUKAMU KEK LONTE');
   	}
-    else if (message.content.toLowerCase() === 'kontol' || message.content.toLowerCase() === 'kntl') {
+    else if (message.content.toLowerCase().includes ('kontol') || ('kntl')) {
     	message.reply('KONTOLMU BELOK');
   	}
-    else if (message.content.toLowerCase() === 'memek' || message.content.toLowerCase() === 'kemem' || message.content.toLowerCase() === 'mmk' || message.content.toLowerCase() === 'mmq') {
+    else if (message.content.toLowerCase().includes ('memek') || ('kemem') || ('mmk') || ('mmq') || ('memeq') || ('memex')) {
     	message.reply('MEMEK MU TEMBEM');
   	}
-    else if (message.content.toLowerCase() === 'kentod') {
+    else if (message.content.toLowerCase().includes ('kentod') || ('kentot') || ('netnot')) {
     	message.reply('MANGGIL LU TOD <@267473212626042882>');
   	}
-    else if (message.content.toLowerCase() === 'nigga' || message.content.toLowerCase() === 'nigg' || message.content.toLowerCase() === 'nigger') {
+    else if (message.content.toLowerCase().includes ('nigga') || ('nigger') || ('nigg') || ('niggward')) {
     	message.reply('GABOLEH RASIS ANJING, YA GAK <@267473212626042882>');
   	}
-    else if (message.content.toLowerCase() === 'aldo' || message.content.toLowerCase() === 'aldog') {
-    	message.reply('ANJING');
+    else if (message.content.toLowerCase().includes ('aldo') || ('aldog')) {
+    	message.reply('<@292889185252278272> ANJING');
   	}
-    else if (message.content.toLowerCase() === 'dipo') {
+    else if (message.content.toLowerCase().includes ('dipo')) {
     	message.reply('CLOUD SERVICE');
   	}
-    else if (message.content.toLowerCase() === 'fuck') {
+    else if (message.content.toLowerCase().includes ('fuck')) {
     	message.reply('AHHH... YAMETE KUDASAIIII');
+  	}
+    else if (message.content.toLowerCase().includes ('asshole') || ('ass hole')) {
+    	message.reply('LOBANG PANTATMU HITAM');
+  	}
+    else if (message.content.toLowerCase().includes ('penis') || ('vagina')) {
+    	message.reply('ALAT NGENTIAW ALAMI');
   	}
 });
 
