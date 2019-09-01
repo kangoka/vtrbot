175363c080b75c9940aadb7b0944b1f0f3d0a1fd
Kang Oka <51883097+kangoka@users.noreply.github.com>
2019-09-01 15:56:09 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+function forceLower(message.content) 
+{
+message.content=message.content.toLowerCase();
+}
+
+
 client.on('ready', () => {
     console.log('TOD BOT IS HERE NJING');
 });
@@ -10,11 +16,6 @@ client.on('message', message => {
     	message.reply('PANG PING PANG PING MATAMU COK');
   	}
 });
-
-function forceLower(message.content) 
-{
-message.content=message.content.toLowerCase();
-}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
