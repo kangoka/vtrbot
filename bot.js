a2dca34117426f4cdc51f35b92dfbb016dd1ed65
Kang Oka <51883097+kangoka@users.noreply.github.com>
2019-09-01 19:00:06 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,6 @@ client.on('ready', () => {
 client.on('message', (message) => {
     if (message.author.bot) return;
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    const cmd = client.commands.get(command);
-    if (!cmd) return;
     
     if (message.content.toLowerCase().includes ('ping') || ('peng')) { 
     	message.reply('BERISIK ANJING');
