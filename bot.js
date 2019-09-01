5e623c85fdf17cdc6b66cb7f34ca151a09c5f9b3
Kang Oka <51883097+kangoka@users.noreply.github.com>
2019-09-01 16:30:45 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.user.setGame("TITIT");
 
 client.on('ready', () => {
     console.log('TOD BOT IS HERE NJING');
 });
+client.user.setGame("TITIT");
 
 client.on('message', message => {
     if (message.content.toLowerCase() === 'ping') {
