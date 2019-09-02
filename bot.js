adb82ac966bde8e511cbaeff978b46f664b75f21
Kang Oka <51883097+kangoka@users.noreply.github.com>
2019-09-02 17:39:12 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
  
 
 client.on("message", async message => {
-  if (message.content.toLowerCase().includes("Dan")){
+  if (message.content.toLowerCase().includes('Dan')){
     // right that is needed to kill the loop ->
     if(message.author.bot) return; 
-    return message.reply("We love you Dan!");
+    return message.reply('We love you Dan!');
     // could also use -> message.channel.send("We love you Dan!")
   } 
 }
