82434a929c095969908ec1fa9a74d4f54ea90f49
Kang Oka <51883097+kangoka@users.noreply.github.com>
2020-01-15 15:56:05 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,47 +1,80 @@
-// Import the discord.js module
 const Discord = require('discord.js');
-
-// Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
-client.on('message', async message => {
-    if (message.author.bot) return;
-    
-    if (message.content.toLowerCase().includes ('ping') || ('peng')) { 
-    	message.reply('BERISIK ANJING');
-  	} else if (message.content.toLowerCase().includes ('asu') || ('asw')) {
-    	    message.reply('MUKELU KEK ASU');
-  	} else if (message.content.toLowerCase().includes ('ajg') || ('anjing') || ('anjeng')) {
-    	    message.reply('MUKELU KEK ANJING');
-  	} else if (message.content.toLowerCase().includes ('ngentot') || ('ngentod') || ('ngntd')) {
-    	    message.reply('NGENTAT NGENTOT, BOCIL MASIH COKIL AE KASAR BET ANJING');
-  	} else if (message.content.toLowerCase().includes ('bgst') || ('bangsat')) {
-    	    message.reply('HEH ANJING KASAR BANGET NGENTOT');
-  	} else if (message.content.toLowerCase().includes ('lonte')) {
-    	    message.reply('LONTE? MUKAMU KEK LONTE');
-  	} else if (message.content.toLowerCase().includes ('kontol') || ('kntl')) {
-    	    message.reply('KONTOLMU BELOK');
-  	} else if (message.content.toLowerCase().includes ('memek') || ('kemem') || ('mmk') || ('mmq') || ('memeq') || ('memex')) {
-            message.reply('MEMEK MU TEMBEM');
-  	} else if (message.content.toLowerCase().includes ('kentod') || ('kentot') || ('netnot')) {
-        	message.reply('MANGGIL LU TOD <@267473212626042882>');
-  	} else if (message.content.toLowerCase().includes ('nigga') || ('nigger') || ('nigg') || ('niggward')) {
-    	    message.reply('GABOLEH RASIS ANJING, YA GAK <@267473212626042882>');
-  	} else if (message.content.toLowerCase().includes ('aldo') || ('aldog')) {
-        	message.reply('<@292889185252278272> ANJING');
-  	} else if (message.content.toLowerCase().includes ('dipo')) {
-    	    message.reply('CLOUD SERVICE');
-  	} else if (message.content.toLowerCase().includes ('fuck')) {
-    	    message.reply('AHHH... YAMETE KUDASAIIII');
-  	} else if (message.content.toLowerCase().includes ('asshole') || ('ass hole')) {
-    	    message.reply('LOBANG PANTATMU HITAM');
-  	} else if (message.content.toLowerCase().includes ('penis') || ('vagina')) {
-        	message.reply('ALAT NGENTIAW ALAMI');
-  	}
+const responseObject = {
+  "ping": "Pong!",
+  "aldo": "anjing"
+};
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("Porn", { type: "WATCHING"});
+});
+
+// Pantau chat
+client.on('message', msg => {
+  if (msg.content.includes ("ping")) {
+    msg.reply('BERISIK ANJING');
+  }
+  else if (msg.content.includes ("aldo") || msg.content.includes ("aldog")) {
+    msg.reply('<@292889185252278272> ANJING');
+  }
+  else if (msg.content.includes ("asu") || msg.content.includes ("asw")) {
+    msg.reply('MUKELU KEK ASU');
+  }
+  else if (msg.content.includes ("anjing") || msg.content.includes ("ajg") || msg.content.includes ("anjeng")) {
+    msg.reply('MUKELU KEK ANJING');
+  }
+  else if (msg.content.includes ("ngentot") || msg.content.includes ("ngentod") || msg.content.includes ("ngntd")) {
+    msg.reply('NGENTAT NGENTOT, BOCIL MASIH COKIL AE KASAR BET ANJING');
+  }
+  else if (msg.content.includes ("bangsat") || msg.content.includes ("bgst") || msg.content.includes ("bngst")) {
+    msg.reply('BANGSAT LO KASAR BANGET');
+  }
+  else if (msg.content.includes ("lonte")) {
+    msg.reply('LONTE? MUKAMU KEK LONTE');
+  }
+  else if (msg.content.includes ("kontol") || msg.content.includes ("kntl")) {
+    msg.reply('KONTOLMU CURVED');
+  }
+  else if (msg.content.includes ("memek") || msg.content.includes ("kemem") || msg.content.includes ("mmk") || msg.content.includes ("mmq") || msg.content.includes ("memeq") || msg.content.includes ("memex")) {
+    msg.reply('NGENTAT NGENTOT, BOCIL MASIH COKIL AE KASAR BET ANJING');
+  }
+  else if (msg.content.includes ("kentod") || msg.content.includes ("kentot") || msg.content.includes ("netnot")) {
+    msg.reply('MANGGIL LU TOD <@267473212626042882>');
+  }
+  else if (msg.content.includes ("nigga") || msg.content.includes ("nigger") || msg.content.includes ("nigg") || msg.content.includes ("niggward") || msg.content.includes ("nigward")) {
+    msg.reply('GABOLEH RASIS ANJING, YA GAK <@267473212626042882>');
+  }
+  else if (msg.content.includes ("dipo")) {
+    msg.reply('CLOUD SERVICE :heart:');
+  }
+  else if (msg.content.includes ("fuck")) {
+    msg.reply('AHHH...YAMETE KUDASAIIII');
+  }
+  else if (msg.content.includes ("asshole")) {
+    msg.reply('LOBANG PANTATMU HITAM');
+  }
+  else if (msg.content.includes ("oka")) {
+    msg.reply('MOST ADORABLE MAN IN THE WORLD :heart:');
+  }
+  else if (msg.content.includes ("wtf")) {
+    msg.reply('WHAT THE FROG?');
+  }
+  else if (msg.content.includes ("jadi gini")) {
+    msg.reply('GIMANA GAN?');
+  }
+  else if (msg.content.includes ("babi")) {
+    msg.channel.send('<@216802455734845442>');
+  }
+});
+
+//Pantau apa ada member masuk
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'datang_akan_pergi');
+  // Send the message, mentioning the member
+  channel.send(`HALO NJING ${member} MET DATANG DI VENTUROUS, GA USAH TERLALU KAKU YA TOD, SANTAI AJA HEHE.`);
 });
 
 // THIS  MUST  BE  THIS  WAY
