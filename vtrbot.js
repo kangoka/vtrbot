b7d69d5192b942e55dbe5e2485fc6a986b6f3cfe
Kang Oka <51883097+kangoka@users.noreply.github.com>
2020-08-20 08:34:49 +0700

    Update vtrbot.js

--- vtrbot.js
+++ vtrbot.js
@@ -30,6 +30,14 @@ client.on('message', msg => {
             m.edit(embed)
         });
   }
+  else if (message.content.toLowerCase() === '!uptime) {
+      let days = Math.floor(client.uptime / 86400000);
+      let hours = Math.floor(client.uptime / 3600000) % 24;
+      let minutes = Math.floor(client.uptime / 60000) % 60;
+      let seconds = Math.floor(client.uptime / 1000) % 60;
+
+      message.channel.send(`__Uptime:__\n${days}d ${hours}h ${minutes}m ${seconds}s`);
+  }
   /*
   else if (msg.content.includes ("aldo") || msg.content.includes ("aldog")) {
     msg.reply('<@292889185252278272> ANJING');
